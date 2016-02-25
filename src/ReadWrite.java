54b39d6a23347b8434d1f7f7264e869f7743004c
omario61 <omario61@hotmail.com>
2016-02-26 00:14:47 +0200

    remove not needed

--- src/ReadWrite.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ReadWrite extends Remote{
-	int [] read (int ID) throws RemoteException;
-	int [] write(int ID) throws RemoteException;
-}
